d261fc470fe0e8c6293f67f083f858766be97735
christian-byrne <abolkonsky.rem@gmail.com>
2024-06-17 21:55:53 -0700

    matrix invertibility equivalence class

---
+++ chapter03_matrix/matrix-invertibility-equivalent-statements.md
@@ -0,0 +1,26 @@
+
+# Matrix Invertibility Equivalent Statements
+
+
+Gathering together the ways we have to detect if a matrix is invertible (or not).
+Let A be a square matrix (n by n). The following statements are equivalent!
+1. A is invertible
+1. Determinant of A is nonzero
+1. rank A=n
+1. Column vectors of A are Linearly Independent
+1. Column vectors of A span n-space
+1. Column vectors of A form a basis for n-space
+1. Av=0 has only the trivial solution (v=[x y z ...] a generic vector).
+1. Av=b has a unique solution for all constant vectors b in n-space
+1. The RREF of A is the n by n identity matrix.
+1. A is the product of elementary matrices.
+1. Kernel of A is the zero vector.
+1. Nullity of A is 0.
+1. A is one-to-one.
+1. A is onto.
+1. Range A is n-space.
+1. rows of A are linearly independent.
+1. rows of A span n-space.
+1. rows of A form a basis for n-space.
+1. 0 is NOT a stretch factor/eigenvalue of A.
+1. 0 is NOT an ellipse factor/singular value of A.
--- use_matlab/main.m
+++ use_matlab/main.m
@@ -1,22 +1,19 @@
 clear;
 clc;
 format rat;
-
-
 syms x;
+syms y;
+syms z;
 
 expr = x^2 - 4*x+4;
 expr_2 = x^2 - 2*x -3;
-
 r = expand(expr * expr_2);
-
 roots(r);
 
 
 A = [
-    2 0 2;
-    0 2 2;
-    2 2 0;
+    0 -2;
+    -3 0;
     ];
 
 B = [
@@ -25,59 +22,15 @@ B = [
     0 0 -1;
     ];
 
-charpoly(A);
+A_characteristic_polynomial = charpoly(A)
+A_eigenvalues = roots(poly(A))
+[A_eigenvectors, A_diagonal] = eig(A)
 
+diagonal_1_1 = A_diagonal(1, 1);
+diagonal_1_2 = A_diagonal(1, 1);
+diagonal_2_1 = A_diagonal(2, 1);
+diagonal_2_2 = A_diagonal(2, 2);
 
-roots(poly(A));
+sqrt(6)
 
-
-[V, D] = eig(B);
-
-C = [
-    -1/2 -1/2 1;
-    1 0 0;
-    0 1 0;
-    ];
-
-C_inv = inv(C);
-
-matrix_m = [
-    -1 0 0;
-    0 -1 0;
-    0 0 1;
-    ];
-
-my_result = C * matrix_m^2027 * C_inv;
-
-matrix_to_compute_power = [
-    1 1 1;
-    0 -1 0;
-    0 0 -1;
-    ];
-
-[mtcp_p, mtcp_d] = eig(matrix_to_compute_power);
-
-matrix_to_compute_power^2027;
-
-
-ex = [
-    -1 1 -1/2;
-    1 1 -1/2;
-    0 1 1;
-    ];
-
-inv(ex);
-
-elipse_example = [
-    sqrt(3)/2 1;
-    -1/2 sqrt(3);
-    ];
-
-[elipse_v, elipse_d] = eig(elipse_example);
-
-disp(elipse_d)
-
-elipse_d(1, 1)
-
-
-sqrt_3 = sqrt(3)
+svd(A)
